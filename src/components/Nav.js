2afc564eedc7f9451bce2bd886ea0d4f931df40a
kate ross <kross@clever-touch.com>
2018-01-04 11:45:39 +0000

    add navlinks

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Nav = props => {
-    return (
-        <nav className="main-nav">
-            <ul>
-              <li><a href='#'>Cats</a></li>
-              <li><a href='#'>Dogs</a></li>
-              <li><a href='#'>Computers</a></li>
-            </ul>
-          </nav>
-    )
-}
-
+const Nav = () => 
+    <nav className="main-nav">
+        <ul>
+            <li><NavLink to='/cats'>Cats</NavLink></li>
+            <li><NavLink to='/dogs'>Dogs</NavLink></li>
+            <li><NavLink to='/computers'>Computers</NavLink></li>
+        </ul>
+    </nav>
 
 export default Nav;
