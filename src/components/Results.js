45d59d60f8743509cd03bf6c3b67b41065691698
kate ross <kross@clever-touch.com>
2018-01-04 11:54:48 +0000

    change props to match

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,16 +5,17 @@ import {
 
 //result components
 import NotFound from './gallery/NotFound';
-import Photo from './gallery/Photo'
+import Photo from './gallery/Photo';
 
-const Results = props => {
+const Results = ({match}) => {
     {/* Get props.results */}
+    let searchQuery = match.params.query;
     return (
         <div class="photo-container">
             <h2>Results</h2>{/* Show if results array > 0 */}
             <ul>
                 {/* If results found */}
-                <Route exact path="/" component={Photo} />
+                <Route exact path="/" render={(props) => {}} component={Photo} />
                 {/* If no results found */}
                 <Route component={NotFound} />
             </ul>
