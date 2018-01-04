cba63ded8f0e04089b86b1ee71292ba088d1d92c
kate ross <kross@clever-touch.com>
2018-01-04 15:03:12 +0000

    Return mapped photos as list item

--- src/components/gallery/Photo.js
+++ src/components/gallery/Photo.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 const Photo = props => {
-    props.photos.map(photo => 
-        <li key={photo.id}>
-            <img src={photo.url} alt="" />
-        </li>
-    )  
+    return (
+        props.photos.map(photo => 
+            <li key={photo.id}>
+                <img src={photo.url} alt="" />
+            </li>
+        ) 
+    )
 }
 
 Photo.propTypes = {
-    id: PropTypes.number.isRequired,
-    url: PropTypes.string.isRequired
+    photos: PropTypes.array.isRequired
 };
 
 export default Photo;
