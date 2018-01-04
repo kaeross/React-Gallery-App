63082e3d5492eec3e2733bf5697f66f7941761b3
kate ross <kross@clever-touch.com>
2018-01-04 11:52:53 +0000

    update get photoUrls function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import {
-  BrowserRouter,
-  Route,
-  Switch
+    BrowserRouter,
+    Route
 } from 'react-router-dom';
 import axios from 'axios';
 
@@ -14,45 +15,44 @@ import apiKey from './config.js';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      photos: []
-    };
-  }
-  
-  componentDidMount(searchTerm) {
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${searchTerm}&format=json&nojsoncallback=1`)
-      .then(response => {
-        this.setState({
-          photos: response.photos.photo
-        });
-      })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error);
-      });
-  }
+    constructor() {
+        super();
+        this.state = {
+            photos: []
+        };
+    }
 
-  getPhotoUrls() {
-    var farmId = this.state.photos.map(photo => {
-      
-    })
-    return `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`
-  }
+    componentDidMount(searchTerm) {
+        axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${searchTerm}&format=json&nojsoncallback=1`)
+            .then(response => {
+                this.setState({
+                    photos: response.photos.photo
+                });
+            })
+            .catch(error => {
+                console.log('Error fetching and parsing data', error);
+            });
+    }
 
-  render() {
-    return (
-      <BrowserRouter>
+    // getPhotoUrls() {
+    //     this.setState.photoUrl = this.state.photos.map(photo => {
+    //         return `https://farm${photo.farmId}.staticflickr.com/${photo.serverId}/${photo.id}_${photo.secret}.jpg`
+    //     });
+    // }
+
+    render() {
+        return ( 
+            <BrowserRouter>
         <div className="App">
           <div className="container">
             <Search />
             <Nav />
             <Route path="/:query" component={Results} />
-          </div>
-        </div>
-      </BrowserRouter>
-    );
-  }
+                </div> 
+            </div> 
+        </BrowserRouter>
+        );
+    }
 }
 
 export default App;
