9b8d5ee1d9ec51f40edb70249bf6ad314e573c85
kaeross <kaeross@icloud.com>
2019-03-03 21:14:38 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, {
     Component
 } from 'react';
-import {
-    Route
-} from 'react-router-dom';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 //Get App Components
 import Results from './components/Results';
@@ -51,28 +49,36 @@ class App extends Component {
         this.getPhotos(this.state.defaultSearchTerm)
     }
 
-    handleSearchInput= e =>
-    this.setState({ searchVal: e.target.value });
+    handleSearchInput= e => this.setState({ searchVal: e.target.value });
 
-    searchSubmitHandler = e => {
+    searchSubmitHandler = e =>  {
         e.preventDefault();
         this.props.history.push(this.state.searchVal);
-        this.setState({ searchVal: '' });
     }
 
     render() {
         return (
-            <div className="App">
-                <div className="container">
-                    <Search 
-                    searchSubmitHandler={this.searchSubmitHandler}
-                    handleSearchInput={this.handleSearchInput}
-                    pendingSearch={this.state.searchVal} />
-                    <Nav />
-                    <Route exact path={'/'} render={ ({match}) => <Results query={this.state.defaultSearchTerm} getPhotos={this.getPhotos} photos={this.state.photos} />} />
-                    <Route path={'/:query'} render={ ({match}) => <Results query={match.params.query} getPhotos={this.getPhotos} photos={this.state.photos} />} />
-                </div> 
-            </div>
+            <Router>
+                <div className="App">
+                    <div className="container">
+                        <Search 
+                            searchSubmitHandler={this.searchSubmitHandler}
+                            handleSearchInput={this.handleSearchInput}
+                            pendingSearch={this.state.searchVal} />
+                        <Nav />
+
+                        <Switch>
+                            <Route exact path={'/'} render={ ({match}) => 
+                                <Results query={this.state.defaultSearchTerm} getPhotos={this.getPhotos} photos={this.state.photos} />
+                            } />
+
+                            <Route path={'/:query'} render={ ({match}) => 
+                                <Results query={match.params.query} getPhotos={this.getPhotos} photos={this.state.photos} />
+                            } />
+                        </Switch>
+                    </div> 
+                </div>
+            </Router>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    BrowserRouter,
-    Route
-} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(( 
-    <BrowserRouter>
-        <Route path={'/'} component={App} />
-    </BrowserRouter>
+    <App />
     ), document.getElementById('root'));
 registerServiceWorker();
