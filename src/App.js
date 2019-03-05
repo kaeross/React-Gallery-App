de99eff0de1242f53c8c7472115d53d18591bf54
kate ross <kross@clever-touch.com>
2019-03-05 13:38:27 +0000

    Localise functionality to specific components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from 'react-router-dom';
 
 //Get App Components
 import Results from './components/Results';
@@ -10,77 +13,83 @@ import Search from './components/Search';
 import Nav from './components/Nav';
 import apiKey from './config.js';
 
+const SearchWithRouter = withRouter(Search);
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      defaultSearchTerm: 'Cat',
+      perPage: 16,
+      photos: []
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            defaultSearchTerm: 'My favourite things',
-            perPage: 16,
-            photos: [],
-            searchVal: ''
-        };
-    }
+  getPhotos = searchTerm => {
+    axios
+      .get(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&per_page=${
+          this.state.perPage
+        }&tags=${searchTerm}&format=json&nojsoncallback=1`
+      )
+      .then(response => {
+        var photoData = response.data.photos.photo;
+        this.setState({
+          photos: photoData.map(photo => {
+            var farmId = photo.farm;
+            var serverId = photo.server;
+            var id = photo.id;
+            var secret = photo.secret;
 
-    getPhotos = searchTerm => {
-        axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&per_page=${this.state.perPage}&tags=${searchTerm}&format=json&nojsoncallback=1`)
-            .then(response => {
-                var photoData = response.data.photos.photo;
-                this.setState({
-                    photos: photoData.map(photo => {
-                        var farmId = photo.farm;
-                        var serverId = photo.server;
-                        var id = photo.id;
-                        var secret = photo.secret;
-                        
-                        return {
-                            id: id,
-                            url: `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`
-                        }
-                    })
-                });
-            })
-            .catch(error => {
-                console.log('Error fetching and parsing data', error);
-            });
-    }
+            return {
+              id: id,
+              url: `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`
+            };
+          })
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  };
 
-    componentDidMount = () =>  {
-        this.getPhotos(this.state.defaultSearchTerm)
-    }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <SearchWithRouter getPhotos={this.getPhotos} />
+            <Nav />
 
-    handleSearchInput= e => this.setState({ searchVal: e.target.value });
+            <Switch>
+              <Route
+                exact
+                path={'/'}
+                render={({ match }) => (
+                  <Results
+                    query={this.state.defaultSearchTerm}
+                    photos={this.state.photos}
+                    getPhotos={this.getPhotos}
+                  />
+                )}
+              />
 
-    searchSubmitHandler = e =>  {
-        e.preventDefault();
-        this.props.history.push(this.state.searchVal);
-    }
-
-    render() {
-        return (
-            <Router>
-                <div className="App">
-                    <div className="container">
-                        <Search 
-                            searchSubmitHandler={this.searchSubmitHandler}
-                            handleSearchInput={this.handleSearchInput}
-                            pendingSearch={this.state.searchVal} />
-                        <Nav />
-
-                        <Switch>
-                            <Route exact path={'/'} render={ ({match}) => 
-                                <Results query={this.state.defaultSearchTerm} getPhotos={this.getPhotos} photos={this.state.photos} />
-                            } />
-
-                            <Route path={'/:query'} render={ ({match}) => 
-                                <Results query={match.params.query} getPhotos={this.getPhotos} photos={this.state.photos} />
-                            } />
-                        </Switch>
-                    </div> 
-                </div>
-            </Router>
-        );
-    }
+              <Route
+                path={'/:query'}
+                render={({ match }) => (
+                  <Results
+                    query={match.params.query}
+                    photos={this.state.photos}
+                    getPhotos={this.getPhotos}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
+import React, { Component } from 'react';
 
 //result components
 import NotFound from './gallery/NotFound';
 import Photo from './gallery/Photo';
 
-const Results = (props) => {
-    //get Photos from url query
-    props.getPhotos(props.query)
+class Results extends Component {
+  componentDidMount() {
+    this.props.getPhotos(this.props.query);
+  }
 
-    if (props.photos.length > 0) {
-        return (
-            <div className="photo-container">
-                <h2>{props.query}</h2>
-                <ul>
-                    <Photo photos={props.photos} /> 
-                </ul>
-            </div>
-        )
+  componentWillUpdate() {
+    this.props.getPhotos(this.props.query);
+  }
+
+  render() {
+    if (this.props.photos.length > 0) {
+      return (
+        <div className="photo-container">
+          <h2>{this.props.query}</h2>
+          <ul>
+            <Photo photos={this.props.photos} />
+          </ul>
+        </div>
+      );
     } else {
-        return (
-            <div className="photo-container">
-                <ul>
-                    <NotFound />
-                </ul>
-            </div>
-        )
+      return (
+        <div className="photo-container">
+          <ul>
+            <NotFound />
+          </ul>
+        </div>
+      );
     }
-    
+  }
 }
-
-Results.propTypes = {
-   photos: PropTypes.array.isRequired 
-};
-
 export default Results;
