697fe3d914e91e40086ab6c87623a936920f9afa
kate ross <kross@clever-touch.com>
2019-03-06 13:13:24 +0000

    Remove api call from main app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import {
   BrowserRouter as Router,
   Route,
@@ -11,7 +10,6 @@ import {
 import Results from './components/Results';
 import Search from './components/Search';
 import Nav from './components/Nav';
-import apiKey from './config.js';
 
 const SearchWithRouter = withRouter(Search);
 
@@ -19,70 +17,29 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      defaultSearchTerm: 'Cat',
-      perPage: 16,
-      photos: []
+      defaultSearchTerm: 'Cat'
     };
   }
-
-  getPhotos = searchTerm => {
-    axios
-      .get(
-        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&per_page=${
-          this.state.perPage
-        }&tags=${searchTerm}&format=json&nojsoncallback=1`
-      )
-      .then(response => {
-        var photoData = response.data.photos.photo;
-        this.setState({
-          photos: photoData.map(photo => {
-            var farmId = photo.farm;
-            var serverId = photo.server;
-            var id = photo.id;
-            var secret = photo.secret;
-
-            return {
-              id: id,
-              url: `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`
-            };
-          })
-        });
-      })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error);
-      });
-  };
 
   render() {
     return (
       <Router>
         <div className="App">
           <div className="container">
-            <SearchWithRouter getPhotos={this.getPhotos} />
+            <SearchWithRouter />
             <Nav />
-
             <Switch>
               <Route
                 exact
                 path={'/'}
                 render={({ match }) => (
-                  <Results
-                    query={this.state.defaultSearchTerm}
-                    photos={this.state.photos}
-                    getPhotos={this.getPhotos}
-                  />
+                  <Results query={this.state.defaultSearchTerm} />
                 )}
               />
 
               <Route
                 path={'/:query'}
-                render={({ match }) => (
-                  <Results
-                    query={match.params.query}
-                    photos={this.state.photos}
-                    getPhotos={this.getPhotos}
-                  />
-                )}
+                render={({ match }) => <Results query={match.params.query} />}
               />
             </Switch>
           </div>
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import apiKey from './../config.js';
 
 //result components
 import NotFound from './gallery/NotFound';
 import Photo from './gallery/Photo';
 
 class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      perPage: 16,
+      photos: []
+    };
+  }
+
+  getPhotos = searchTerm => {
+    axios
+      .get(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&per_page=${
+          this.state.perPage
+        }&tags=${searchTerm}&format=json&nojsoncallback=1`
+      )
+      .then(response => {
+        var photoData = response.data.photos.photo;
+        this.setState({
+          photos: photoData.map(photo => {
+            var farmId = photo.farm;
+            var serverId = photo.server;
+            var id = photo.id;
+            var secret = photo.secret;
+
+            return {
+              id: id,
+              url: `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`
+            };
+          })
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  };
+
   componentDidMount() {
-    this.props.getPhotos(this.props.query);
+    this.getPhotos(this.props.query);
   }
 
   componentWillUpdate() {
-    this.props.getPhotos(this.props.query);
+    this.getPhotos(this.props.query);
   }
 
   render() {
-    if (this.props.photos.length > 0) {
+    if (this.state.photos.length > 0) {
       return (
         <div className="photo-container">
           <h2>{this.props.query}</h2>
           <ul>
-            <Photo photos={this.props.photos} />
+            <Photo photos={this.state.photos} />
           </ul>
         </div>
       );
